e7ddbb7b16b1a9024900072605503356600fc8ac
David White <david.white@changemakerdata.nyc>
2026-02-15 12:13:21 -0500

    Added housing quote graphic

--- notebooks/citytracker.py
+++ notebooks/citytracker.py
@@ -299,15 +299,18 @@ def _(mo):
     return
 
 
-@app.cell
-def _():
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ---
+    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""
-    ---
+    ## Issue #1: Housing Affordability
     """)
     return
 
